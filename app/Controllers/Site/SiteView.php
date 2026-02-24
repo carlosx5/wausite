afaba2271c8a164f27289a3fd96d82868aa7e654
carlosx5 <carlosvieirax5@email.com>
2026-02-24 18:48:36 -0300

    Troca de nomes dos arquivos

--- app/Controllers/Site/SiteView.php
+++ app/Controllers/Site/SiteView.php
@@ -13,11 +13,11 @@ class SiteView extends UnloggedController
         $data['video'] = "https://vz-09ddef2b-e28.b-cdn.net/1ceea333-3af2-46a8-bf4d-66f9f5c05c68/playlist.m3u8";
         $data['logo'] = base_url("dataSistem/images/logos/wau/wau300x117_2.webp?v=$dv");
         ///
-        $data['content02_1'] = base_url("dataSistem/images/content02/content02_1.webp?v=$dv");
-        $data['content02_2'] = base_url("dataSistem/images/content02/content02_2.webp?v=$dv");
-        $data['content02_3'] = base_url("dataSistem/images/content02/content02_3.webp?v=$dv");
+        $data['rowTop_1'] = base_url("dataSistem/images/rowTop/rowTop_1.webp?v=$dv");
+        $data['rowTop_2'] = base_url("dataSistem/images/rowTop/rowTop_2.webp?v=$dv");
+        $data['rowTop_3'] = base_url("dataSistem/images/rowTop/rowTop_3.webp?v=$dv");
         ///
-        $data['content03_1'] = base_url("dataSistem/images/content03/content03_1.webp?v=$dv");
+        $data['mapa_1'] = base_url("dataSistem/images/mapa/mapa_1.webp?v=$dv");
 
         echo view('site/main.html', $data);
     }
