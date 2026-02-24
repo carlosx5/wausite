859fdbec1323912732e7bf4278062601527eec2c
carlosx5 <carlosvieirax5@email.com>
2026-02-23 21:21:05 -0300

    Content02 - 2

--- app/Controllers/Site/SiteView.php
+++ app/Controllers/Site/SiteView.php
@@ -8,13 +8,14 @@ class SiteView extends UnloggedController
 {
     public function index()
     {
-        $data['refresh'] = uniqid();
-        $data['video'] = base_url('dataSistem/videos/videotop01.mp4?v=') . $data['refresh'];
-        $data['logo'] = base_url('dataSistem/images/logos/wau/wau300x117_2.webp?v=') . $data['refresh'];
+        $dv = uniqid(); //.dv = data version
+        $data['refresh'] = $dv;
+        $data['video'] = base_url("dataSistem/videos/videotop01.mp4?v=$dv");
+        $data['logo'] = base_url("dataSistem/images/logos/wau/wau300x117_2.webp?v=$dv");
 
-        $data['content02_1'] = base_url('dataSistem/images/content02/content02_1.webp?v=') . $data['refresh'];
-        $data['content02_2'] = base_url('dataSistem/images/content02/content02_2.webp?v=') . $data['refresh'];
-        $data['content02_3'] = base_url('dataSistem/images/content02/content02_3.webp?v=') . $data['refresh'];
+        $data['content02_1'] = base_url("dataSistem/images/content02/content02_1.webp?v=$dv");
+        $data['content02_2'] = base_url("dataSistem/images/content02/content02_2.webp?v=$dv");
+        $data['content02_3'] = base_url("dataSistem/images/content02/content02_3.webp?v=$dv");
 
         echo view('site/main.html', $data);
     }
